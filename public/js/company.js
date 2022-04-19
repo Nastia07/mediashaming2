181ed9c457100a653bbef071ca377dc0291bfddd
Nastia_Chulupyn <nculupin@gmail.com>
2022-04-19 14:21:04 +0300

    added nespers

--- public/js/company.js
+++ public/js/company.js
@@ -6,5 +6,38 @@ document.addEventListener("DOMContentLoaded", function (event) {
     twitter_link.href = "https://twitter.com/intent/tweet?text=".concat(text);
   }
 
-  setTwiterLink();
+  if (twitter_link != undefined) {
+    setTwiterLink();
+  }
+
+  const swiperLength =
+    document.getElementsByClassName("activity__slider").length;
+  if (swiperLength > 0) {
+    let swiper = new Swiper(".activity__slider", {
+      watchOverflow: true,
+      loop: true,
+      speed: 3500,
+      autoplay: {
+        delay: 0,
+      },
+      spaceBetween: 10,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+
+        700: {
+          slidesPerView: 2,
+        },
+
+        900: {
+          slidesPerView: 3,
+        },
+
+        1280: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
 });
